2ff7c5461a2895a2d586f92a33f8cfb8635f8b5e
Gui55 <gui.mergulhao55@outlook.com>
2023-04-03 14:18:44 -0300

    Organização de packages e reconhecimento do modulo Core pelo gradle

--- app/src/main/java/com/example/neosuperjourney/presentation/MainActivity.kt
+++ app/src/main/java/com/example/neosuperjourney/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.neosuperjourney
+package com.example.neosuperjourney.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.neosuperjourney.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
